e72495c53e406f51c25e22a5219b8b20899875f8
lqiang2003cn <randycoq@163.com>
2024-12-23 23:19:34 +0800

    o

--- docs/notebooks/active_inference_from_scratch.py
+++ docs/notebooks/active_inference_from_scratch.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 
@@ -60,27 +60,28 @@ def plot_beliefs(belief_dist, title_str=""):
     plt.show()
 
 
-# Category Distribution
 from pymdp import utils
 
 my_categorical = np.random.rand(3)
 # my_categorical = np.array([0.5, 0.3, 0.8]) # could also just write in your own numbers
 my_categorical = utils.norm_dist(my_categorical)  # normalizes the distribution so it integrates to 1.0
+
 print(my_categorical.reshape(-1, 1))  # we reshape it to display it like a column vector
 print(f'Integral of the distribution: {round(my_categorical.sum(), 2)}')
+
 sampled_outcome = utils.sample(my_categorical)
 print(f'Sampled outcome: {sampled_outcome}')
 plot_beliefs(my_categorical, title_str="A random (unconditional) Categorical distribution")
 
-# Conditional Distribution
 # initialize it with random numbers
 p_x_given_y = np.random.rand(3, 4)
 print(p_x_given_y.round(3))
-# normalize it: by default normalize the first axis
+# normalize it: normalize first dim by default
 p_x_given_y = utils.norm_dist(p_x_given_y)
 print(p_x_given_y.round(3))
 print(p_x_given_y[:, 0].reshape(-1, 1))
 print(f'Integral of P(X|Y=0): {p_x_given_y[:, 0].sum()}')
+
 """ Create a P(Y) and P(X|Y) using the same numbers from the slides """
 p_y = np.array([0.75, 0.25])  # this is already normalized - you don't need to `utils.norm_dist()` it!
 # the columns here are already normalized - you don't need to `utils.norm_dist()` it!
@@ -92,26 +93,26 @@ print(p_x_given_y.round(3))
 """ Calculate the expectation using numpy's dot product functionality """
 # first version of the dot product (using the method of a numpy array)
 E_x_wrt_y = p_x_given_y.dot(p_y)
+# second version of the dot product (using the function np.dot with two arguments)
+# E_x_wrt_y = np.dot(p_x_given_y, p_y)
 print(E_x_wrt_y)
 print(f'Integral: {E_x_wrt_y.sum().round(3)}')
+
 import itertools
 
 """ Create  the grid locations in the form of a list of (Y, X) tuples -- HINT: use itertools """
 grid_locations = list(itertools.product(range(3), repeat=2))
 print(grid_locations)
 plot_grid(grid_locations)
+
 """ Create variables for the storing the dimensionalities of the hidden states and the observations """
 n_states = len(grid_locations)
 n_observations = len(grid_locations)
 print(f'Dimensionality of hidden states: {n_states}')
 print(f'Dimensionality of observations: {n_observations}')
-""" Create the A matrix  """
-A = np.zeros((n_states, n_observations))
-""" Create an umambiguous or 'noise-less' mapping between hidden states and observations """
+A = np.zeros((n_observations, n_states))
 np.fill_diagonal(A, 1.0)
 plot_likelihood(A, title_str="A matrix or $P(o|s)$")
-""" Remind yourself of the mapping between linear indices (0 through 8) and grid locations (Y, X) """
-plot_grid(grid_locations)
 A_noisy = A.copy()
 # this line says: the probability of seeing yourself in location 0, given you're in location 0, is 1/3, AKA P(o == 0 | s == 0) = 0.3333....
 A_noisy[0, 0] = 1 / 3.0  # corresponds to location (0,0)
@@ -120,13 +121,14 @@ A_noisy[1, 0] = 1 / 3.0  # corresponds to one step to the right from (0, 1)
 # this line says: the probability of seeing yourself in location 3, given you're in location 0, is 1/3, AKA P(o == 3 | s == 0) = 0.3333....
 A_noisy[3, 0] = 1 / 3.0  # corresponds to one step down from (1, 0)
 plot_likelihood(A_noisy, title_str='modified A matrix where location (0,0) is "blurry"')
-""" Let's make ake one grid location "ambiguous" in the sense that it could be easily confused with neighbouring locations """
 my_A_noisy = A_noisy.copy()
+
 # locations 3 and 7 are the nearest neighbours to location 6
 my_A_noisy[3, 6] = 1.0 / 3.0
 my_A_noisy[6, 6] = 1.0 / 3.0
 my_A_noisy[7, 6] = 1.0 / 3.0
 plot_likelihood(my_A_noisy, title_str="Noisy A matrix now with TWO ambiguous locations")
+
 actions = ["UP", "DOWN", "LEFT", "RIGHT", "STAY"]
 
 
@@ -157,59 +159,31 @@ def create_B_matrix():
 
 
 B = create_B_matrix()
-starting_location = (1, 0)
+""" Define a starting location"""
+starting_location = (1,0)
+"""get the linear index of the state"""
 state_index = grid_locations.index(starting_location)
+"""  and create a state vector out of it """
 starting_state = utils.onehot(state_index, n_states)
 plot_point_on_grid(starting_state, grid_locations)
 plot_beliefs(starting_state, "Categorical distribution over the starting state")
+""" Generate the next state vector, given the starting state and the B matrix"""
 right_action_idx = actions.index("RIGHT")
-next_state = B[:, :, right_action_idx].dot(starting_state)  # input the indices to the B matrix
+next_state = B[:,:, right_action_idx].dot(starting_state) # input the indices to the B matrix
+""" Plot the next state, after taking the action """
 plot_point_on_grid(next_state, grid_locations)
 prev_state = next_state.copy()
 down_action_index = actions.index("DOWN")
-next_state = B[:, :, down_action_index].dot(prev_state)
+next_state = B[:,:,down_action_index].dot(prev_state)
+"""  Plot the new state vector, after making the movement """
 plot_point_on_grid(next_state, grid_locations)
 
+
+""" Create an empty vector to store the preferences over observations """
 C = np.zeros(n_observations)
-desired_location = (2, 2)  # choose a desired location
-desired_location_index = grid_locations.index(desired_location)
-C[desired_location_index] = 1.0  # set the preference for that location to be 100%, i.e. 1.0
-plot_beliefs(C, title_str="Preferences over observations")
-
-# create a one-hot / certain belief about initial state
-D = utils.onehot(0, n_states)
-# demonstrate hwo belief about initial state can also be uncertain / spread among different possible initial states
-# alternative, where you have a degenerate/noisy prior belief
-# D = utils.norm_dist(np.ones(n_states))
-""" Let's look at the prior over hidden states """
-plot_beliefs(D, title_str="Prior beliefs over states")
-
-from pymdp.maths import softmax
-from pymdp.maths import spm_log_single as log_stable
-
-
-def infer_states(observation_index, A, prior):
-    log_likelihood = log_stable(A[observation_index, :])
-    log_prior = log_stable(prior)
-    qs = softmax(log_likelihood + log_prior)
-    return qs
-
-################### A simple inference ###################
-qs_past = utils.onehot(4, n_states)  # agent believes they were at location 4 -- i.e. (1,1) one timestep ago
-last_action = "UP"  # the agent knew it moved "UP" one timestep ago
-action_id = actions.index(last_action)  # get the action index for moving "UP"
-prior = B[:, :, action_id].dot(qs_past)
-observation_index = 1
-qs_new = infer_states(observation_index, A, prior)
-plot_beliefs(qs_new, title_str = "Beliefs about hidden states")
-################### A simple inference ###################
-
-observation_index = 8 # this is like the agent is seeing itself in location (0, 2)
-qs_new = infer_states(observation_index, A, prior)
-plot_beliefs(qs_new)
-A_partially_ambiguous = softmax(A)
-print(A_partially_ambiguous.round(3))
-noisy_prior = softmax(prior)
-plot_beliefs(noisy_prior)
-qs_new = infer_states(observation_index, A_partially_ambiguous, noisy_prior)
-plot_beliefs(qs_new)
+""" Choose an observation index to be the 'desired' rewarding index, and fill out the C vector accordingly """
+desired_location = (2,2) # choose a desired location
+desired_location_index = grid_locations.index(desired_location) # get the linear index of the grid location, in terms of 0 through 8
+C[desired_location_index] = 1.0 # set the preference for that location to be 100%, i.e. 1.0
+"""  Let's look at the prior preference distribution """
+plot_beliefs(C, title_str = "Preferences over observations")
